2037f81187658523a6f9a9a42dd077b166683f47
Saad Khan <saadkhaan1002@gmail.com>
2024-10-18 22:11:27 +0500

    feature 3

--- UI.py
+++ UI.py
@@ -1,12 +1,13 @@
-import features
 from features import per_addition, per_subtract
 from feature2 import mul_tply
+from feature3 import area_of_rect
 def app_UI():
       print("**********Welcome to Calclator app***********")
       print("""Operations available are
        1.Addition
        2.Subtraction
        3. Multiply
+       4. Area of rectangle
             """)
 def num_bers():
       inp_1=int(input("Enter first number: "))
@@ -20,6 +21,8 @@ def func_tions(inp_1, inp_2):
             return per_subtract(inp_1, inp_2)
       elif user_inp==3:
             return mul_tply(inp_1, inp_2)
+      elif user_inp==4:
+            return area_of_rect(inp_1, inp_2)
       else:
             print("invalid operation")
 app_UI()
---
+++ feature3.py
@@ -0,0 +1,2 @@
+def area_of_rect(a,b):
+    return a*b
